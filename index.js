9f892a913f1028d04cb0099ca278ebf3bb014698
Adnan Issadeen <eddyrox1@hotmail.com>
2021-05-12 11:19:12 +0530

    Mail can now be sorted
    Using a CLI prompt, we can select whether we want to screen in the email (And
    where to). We can also choose to ignore the mail for now, or screen it out

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ImapFlow } = require('imapflow');
-
+const prompt = require('prompt');
+const fs = require('fs');
 const config = require('./config.prod');
 const client = new ImapFlow({
   host: config.host,
@@ -12,7 +13,8 @@ const client = new ImapFlow({
   logger: {}
 });
 
-let sortedMailConfig = {};
+let sortedMailConfig = JSON.parse(fs.readFileSync('sorted-mail.json'))
+let ignoreMail = {}
 const getMail = async () => {
   console.log('Connecting to client')
   await client.connect();
@@ -28,12 +30,41 @@ const getMail = async () => {
     console.log(message.envelope.from);
     console.log('Fetching all unread mail');
     for await (let msg of client.fetch('1:*', {envelope: true})){
-      console.log(msg.uid);
-      console.log(msg.envelope.subject);
-      console.log(msg.envelope.from);
+      fromAddress = msg.envelope.from[0].address
+      console.log(`-----------------------------------------------------------
+UID: ${msg.uid}, Seq: ${msg.seq}
+
+Subject: "${msg.envelope.subject}"
+
+Sender: ${msg.envelope.from[0].name} (${msg.envelope.from[0].address})`);
+      prompt.start();
+      prompt.message = '';
+      if (sortedMailConfig[fromAddress] === undefined && ignoreMail[fromAddress] === undefined){
+        let { screenOption } = await prompt.get({
+          name: 'screenOption',
+          description: `What do you want to do for this sender?
+  o (screen out)
+  c(screen to conversations)
+  f (screen to feed)
+  p (screen to paper trail)
+  i (ignore for now)`
+        });
+        if (screenOption !== 'i') {
+          sortedMailConfig[msg.envelope.from[0].address] = screenOption;
+        } else {
+          ignoreMail[msg.envelope.from[0].address] = screenOption;
+        }
+      } else {
+        if (sortedMailConfig[fromAddress] !== undefined) {
+          console.log(`Option chosen for the sender is ${sortedMailConfig[msg.envelope.from[0].address]}`)
+        } else {
+          console.log('Choosing to ignore this sender for this session');
+        }
+      }
     }
   } finally {
-    console.log('Closing mailbox');
+    fs.writeFileSync('sorted-mail.json', JSON.stringify(sortedMailConfig));
+    console.log('\nClosing mailbox');
     await client.mailboxClose();
     console.log('Mailbox closed');
   }
